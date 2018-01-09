0ee5ceca86cb1af880086bf5433b0a2b49162c3c
Timothé <timothesauvage@gmail.com>
2018-01-09 10:51:04 +0100

    Connect test

--- Projet/src/mainActivity.java
+++ Projet/src/mainActivity.java
@@ -1,6 +1,13 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Enumeration;
+import java.util.Properties;
 
+import java.util.Connect;
 public class mainActivity {
 	public static void main(String[] args){
 		  //Contenu de votre classe
+		static Connect Connexion;
+		
 		}
 }
